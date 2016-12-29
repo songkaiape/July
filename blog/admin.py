da2852a56465927a7f31d8bdfeb47b592edc49bf
ansheng <ansheng@anshengdeMacBook-Air.local>
2016-12-29 15:13:38 +0800

    修改lib目录为plugin

--- blog/admin.py
+++ blog/admin.py
@@ -20,12 +20,12 @@ class ArchiveAdmin(admin.ModelAdmin):
 
     class Media:
         css = {
-            'all': ('/static/lib/editor.md/editormd.css',)
+            'all': ('/static/plugin/editor.md/editormd.css',)
         }
         js = (
-            '/static/lib/editor.md/jquery.min.js',
-            '/static/lib/editor.md/editormd.js',
-            '/static/lib/editor.md/config.js'
+            '/static/plugin/editor.md/jquery.min.js',
+            '/static/plugin/editor.md/editormd.js',
+            '/static/plugin/editor.md/config.js'
         )
 
 
