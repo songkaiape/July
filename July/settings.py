296af76fffb5099767cb969f23b5b69167595a1e
ansheng <anshengme.com@gmail.com>
2017-01-12 16:42:05 +0800

    默认使用sqlite3

--- July/settings.py
+++ July/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'July.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'july',
-        'USER': 'root',
-        'PASSWORD': 'ansheng.me',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
